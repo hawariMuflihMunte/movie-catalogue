dda62fafef3a13db935a9bd65dd6e7fcd2d30365
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-10 09:46:48 +0700

    Create simple router

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,12 @@
+const Detail = {
+  async render () {
+    return `
+      <h2>Detail Page</h2>
+    `
+  },
+  async afterRender () {
+    // Fungsi ini akan dipanggil setelah render()
+  }
+}
+
+export default Detail
---
+++ src/scripts/views/pages/upcoming.js
@@ -0,0 +1,12 @@
+const Upcoming = {
+  async render () {
+    return `
+      <h2>Upcoming Page</h2>
+    `
+  },
+  async afterRender () {
+    // Fungsi ini akan dipanggil setelah render()
+  }
+}
+
+export default Upcoming
