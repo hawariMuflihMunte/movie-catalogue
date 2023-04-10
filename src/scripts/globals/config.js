9c34c6ec5f3ed6fad431458ed11858491b8069be
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-10 11:19:59 +0700

    Vanilla route (#1)
    * Create simple router
    
    * Fetch API
    + Fetch data from https://themoviedb.org
    
    * Integrate pages with templates
    
    * Remove unused lines of codes
    
    * Display content
    
    * Fix return response
    
    * Add Icons
    
    * app.webmanifest

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '74740170f35170b474bdc2aeab5e8096',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us'
+}
+
+export default CONFIG
---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,19 @@
+import UrlParser from '../../routes/url-parser'
+import TheMovieDbSource from '../../data/themoviedb-source'
+import { createMovieDetailTemplate } from '../templates/template-creator'
+
+const Detail = {
+  async render () {
+    return `
+      <div id="movie" class="movie"></div>
+    `
+  },
+  async afterRender () {
+    const url = UrlParser.parseActiveUrlWithoutCombiner()
+    const movie = await TheMovieDbSource.detailMovie(url.id)
+    const movieContainer = document.querySelector('#movie')
+    movieContainer.innerHTML = createMovieDetailTemplate(movie)
+  }
+}
+
+export default Detail
