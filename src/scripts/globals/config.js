629ec78d4fa38330d3b4f03f3e5cc345fb8af5dd
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-12 00:52:20 +0700

    Hardcode CACHE_NAME

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,8 @@ const CONFIG = {
   KEY: '74740170f35170b474bdc2aeab5e8096',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-  DEFAULT_LANGUAGE: 'en-us'
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1'
 }
 
 export default CONFIG
