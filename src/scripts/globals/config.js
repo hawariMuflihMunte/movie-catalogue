4a6932abe228328ba7ff8c8766454854757ac46c
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-12 01:04:41 +0700

    Create dynamic value for CACHE_NAME

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: 'MovieCatalogue-V1'
+  CACHE_NAME: new Date().toISOString()
 }
 
 export default CONFIG
