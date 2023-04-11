bc1dfc83ddbeba5fd6617afa19ad4d78d1ec53bf
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-11 22:00:46 +0700

    Create safer method to register service worker

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,5 +1,15 @@
 const swRegister = async () => {
-  navigator.serviceWorker.register('./sw.bundle.js')
+  if (!('serviceWorker' in navigator)) {
+    console.log('Service worker not supported in the browser')
+    return false
+  }
+
+  try {
+    await navigator.serviceWorker.register('./sw.bundle.js')
+    console.log('Service worker registered')
+  } catch (error) {
+    console.log('Failed to register service worker', error)
+  }
 }
 
 export default swRegister
