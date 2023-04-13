0f71fd5bfa1edf37490e1ba8506ddc6de76418f3
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-13 22:46:39 +0700

    Apply logic to websocket-initiator.js

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -1,5 +1,12 @@
 const WebSocketInitiator = {
-
+  init (url) {
+    const webSocket = new WebSocket(url)
+    webSocket.onmessage = this._onMessageHandler
+  },
+  _onMessageHandler (message) {
+    console.log(message.data)
+    console.log(JSON.parse(message.data))
+  }
 }
 
 export default WebSocketInitiator
