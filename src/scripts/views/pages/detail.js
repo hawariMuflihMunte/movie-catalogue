18f69f3c76f987154dd956ea343cfa558a3a0e0f
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-13 12:12:20 +0700

    Add buttons for detail.js + Like button

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,18 +1,22 @@
 import UrlParser from '../../routes/url-parser'
 import TheMovieDbSource from '../../data/themoviedb-source'
-import { createMovieDetailTemplate } from '../templates/template-creator'
+import { createLikeButtonTemplate, createMovieDetailTemplate } from '../templates/template-creator'
 
 const Detail = {
   async render () {
     return `
       <div id="movie" class="movie"></div>
+      <div id="likeButtonContainer"></div>
     `
   },
   async afterRender () {
     const url = UrlParser.parseActiveUrlWithoutCombiner()
     const movie = await TheMovieDbSource.detailMovie(url.id)
     const movieContainer = document.querySelector('#movie')
+    const likeButtonContainer = document.querySelector('#likeButtonContainer')
+
     movieContainer.innerHTML = createMovieDetailTemplate(movie)
+    likeButtonContainer.innerHTML = createLikeButtonTemplate()
   }
 }
 
