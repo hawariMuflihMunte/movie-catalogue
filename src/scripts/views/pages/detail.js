51bea20e1da0063f135db829e2cd35eae56232f3
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-13 12:35:31 +0700

    Logic for like button

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,6 +1,7 @@
 import UrlParser from '../../routes/url-parser'
 import TheMovieDbSource from '../../data/themoviedb-source'
 import { createLikeButtonTemplate, createMovieDetailTemplate } from '../templates/template-creator'
+import LikeButtonInitiator from '../../utils/like-button-initiator'
 
 const Detail = {
   async render () {
@@ -13,10 +14,18 @@ const Detail = {
     const url = UrlParser.parseActiveUrlWithoutCombiner()
     const movie = await TheMovieDbSource.detailMovie(url.id)
     const movieContainer = document.querySelector('#movie')
-    const likeButtonContainer = document.querySelector('#likeButtonContainer')
-
     movieContainer.innerHTML = createMovieDetailTemplate(movie)
-    likeButtonContainer.innerHTML = createLikeButtonTemplate()
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdrop_path: movie.backdrop_path,
+        vote_average: movie.vote_average
+      }
+    })
   }
 }
 
