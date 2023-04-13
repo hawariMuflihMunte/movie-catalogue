1987a0612a863995d9bd88dc05a61c652e1aa0f5
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-13 12:05:06 +0700

    Add IndexedDB methods

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,10 @@ const CONFIG = {
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: new Date().toISOString()
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies'
 }
 
 export default CONFIG
