8de511b76221d481630e8326ec45d7aec460c470
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-13 11:15:24 +0700

    Implement Workbox + Delete manual registration for workbox + delete sw.js, cache-helper.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 const path = require('path')
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js'),
-    sw: path.resolve(__dirname, 'src/scripts/sw.js')
+    app: path.resolve(__dirname, 'src/scripts/index.js')
   },
   output: {
     filename: '[name].bundle.js',
@@ -39,6 +39,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/')
         }
       ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js'
     })
   ]
 }
