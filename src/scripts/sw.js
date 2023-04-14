69606d365d8b3baae717a5e19cef3ce8b6587543
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-15 01:07:09 +0700

    Fix a typo

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,7 +1,7 @@
 import { precacheAndRoute } from 'workbox-precaching'
 
 // Do precaching
-precacheAndRoute(self.__WB__MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST)
 
 self.addEventListener('install', () => {
   console.log('Service worker: Installed')
