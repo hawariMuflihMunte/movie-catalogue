6f86b8ab37fe05f06464871a059ebfd9ccee610b
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-15 00:40:52 +0700

    Implement Push Notification

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,7 @@ import App from './views/app'
 import swRegister from './utils/sw-register'
 import WebSocketInitiator from './utils/websocket-initiator'
 import CONFIG from './globals/config'
+import FooterToolsInitiator from './utils/footer-tools-initiator'
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -20,4 +21,10 @@ window.addEventListener('load', () => {
   app.renderPage()
   swRegister()
   WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER)
+
+  // Initialize footer tools
+  FooterToolsInitiator.init({
+    subscribeButton: document.querySelector('#subscribePushNotification'),
+    unsubscribeButton: document.querySelector('#unsubscribePushNotification')
+  })
 })
