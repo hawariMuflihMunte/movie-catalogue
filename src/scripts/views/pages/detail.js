a311f982360ad4398e17b78a5555a352b155ed89
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-03 14:27:50 +0700

    Fix a typo + Typo in import statement

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,7 +1,7 @@
 import UrlParser from '../../routes/url-parser'
 import TheMovieDbSource from '../../data/themoviedb-source'
 import { createMovieDetailTemplate } from '../templates/template-creator'
-import LikeButtonPresenter from '../../utils/like-button-initiator'
+import LikeButtonPresenter from '../../utils/like-button-presenter'
 import FavoriteMovieIdb from '../../data/favorite-movie-idb'
 
 const Detail = {
