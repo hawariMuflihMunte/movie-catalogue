6802548eff5e65f4daf718397db76e60c50790a7
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-03 13:14:34 +0700

    Update project with addition of  `favoriteMovies` parameter and integrate it with testing + Update import statements `detail.js` + Update import statements `testFactories.js`

--- specs/helpers/testFactories.js
+++ specs/helpers/testFactories.js
@@ -1,8 +1,10 @@
-import LikeButtonInitiator from '../../src/scripts/utils/like-button-presenter'
+import LikeButtonPresenter from '../../src/scripts/utils/like-button-presenter'
+import FavoriteMovieIdb from '../../src/scripts/data/favorite-movie-idb'
 
 const createLikeButtonPresenterWithMovie = async (movie) => {
-  await LikeButtonInitiator.init({
+  await LikeButtonPresenter.init({
     likeButtonContainer: document.querySelector('#likeButtonContainer'),
+    favoriteMovies: FavoriteMovieIdb,
     movie
   })
 }
