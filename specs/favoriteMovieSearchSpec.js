6a2406f6a64a5b56b6d534d77801ec44d67e9d61
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-03 14:42:35 +0700

    Update testing code in `favoriteMovieSearchSpec.js`

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -1,6 +1,23 @@
 /* eslint-disable no-undef */
 describe('Searching movies', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div id="movie-search-container">
+        <input id="query" type="text">
+        <div class="movie-result-container">
+          <ul class="movies">
+          </ul>
+        </div>
+      </div>
+    `
+  })
+
   it('Should be able to capture the query typed by the user', () => {
-    
+    const queryElement = document.getElementById('query')
+    queryElement.value = 'film a'
+    queryElement.dispatchEvent(new Event('change'))
+
+    expect((new FavoriteMovieSearchPresenter()).userQuery)
+      .toEqual('film a')
   })
 })
