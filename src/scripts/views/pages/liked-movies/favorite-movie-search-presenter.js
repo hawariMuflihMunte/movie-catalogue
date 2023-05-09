7513706a304466ddfd0093a519e8fe6549f42a0d
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-09 16:10:20 +0700

    Add empty favorite movie return response

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -33,17 +33,32 @@ class FavoriteMovieSearchPresenter {
       return false
     }
 
-    console.log(movies)
-    const html = movies.reduce(
-      (carry, movie) => carry.concat(`
-        <li class="movie">
-          <span class="movie__title">${movie.title || '-'}</span>
-        </li>
-      `),
-      ''
-    )
+    /**
+     * For debugging purposes only
+     *
+     * Remove this comment below if
+     * you want to see what data is
+     * passed
+     */
+    // console.log(movies)
+
+    let html
+
+    if (movies.length > 0) {
+      html = movies.reduce(
+        (carry, movie) => carry.concat(`
+          <li class="movie">
+            <span class="movie__title">${movie.title || '-'}</span>
+          </li>
+        `),
+        ''
+      )
+    } else {
+      html = '<div class="movies__not__found">Film tidak ditemukan</div>'
+    }
 
     document.querySelector('.movies').innerHTML = html
+
     document.getElementById('movie-search-container')
       .dispatchEvent(new Event('movies:searched:updated'))
   }
