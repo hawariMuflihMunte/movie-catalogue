4a969aacc5eea92cf760ccf485a5eeb16860999c
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-09 10:00:35 +0700

    Refactor code and features improvement + Improvement in `_searchMovies` + Improvement in `_showFoundMovies`

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -14,12 +14,16 @@ class FavoriteMovieSearchPresenter {
     })
   }
 
-  _searchMovies (latestQuery) {
+  async _searchMovies (latestQuery) {
     this._latestQuery = latestQuery
-    this._favoriteMovies.searchMovies(this.latestQuery)
+
+    const foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery)
+
+    this._showFoundMovies(foundMovies)
   }
 
   _showFoundMovies (movies) {
+    console.log(movies)
     const html = movies.reduce(
       (carry, movie) => carry.concat(`
         <li class="movie">
@@ -28,7 +32,10 @@ class FavoriteMovieSearchPresenter {
       `),
       ''
     )
+
     document.querySelector('.movies').innerHTML = html
+    document.getElementById('movie-search-container')
+      .dispatchEvent(new Event('movies:searched:updated'))
   }
 
   get latestQuery () {
