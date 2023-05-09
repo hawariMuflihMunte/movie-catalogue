c9a5e56c6fff4a0e3f12b363736cae0ac3861ae6
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-09 17:16:44 +0700

    Remove `FavoriteMovieSearchView` class

--- specs/favoriteMovieSearchSpec.js
+++ specs/favoriteMovieSearchSpec.js
@@ -1,63 +1,8 @@
 /* eslint-disable no-undef */
 
 import FavoriteMovieSearchPresenter from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter'
+import FavoriteMovieSearchView from '../src/scripts/views/pages/liked-movies/favorite-movie-search-view'
 import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb'
-
-class FavoriteMovieSearchView {
-  getTemplate () {
-    return `
-      <div id="movie-search-container">
-        <input id="query" type="text">
-        <div class="movie-result-container">
-          <ul class="movies">
-          </ul>
-        </div>
-      </div>
-    `
-  }
-
-  runWhenUserIsSearching (callback) {
-    document.getElementById('query')
-      .addEventListener('change', (event) => {
-        callback(event.target.value)
-      })
-  }
-
-  showMovies (movies) {
-    if (!movies) {
-      return false
-    }
-
-    /**
-     * For debugging purposes only
-     *
-     * Remove this comment below if
-     * you want to see what data is
-     * passed
-     */
-    // console.log(movies)
-
-    let html
-
-    if (movies.length > 0) {
-      html = movies.reduce(
-        (carry, movie) => carry.concat(`
-          <li class="movie">
-            <span class="movie__title">${movie.title || '-'}</span>
-          </li>
-        `),
-        ''
-      )
-    } else {
-      html = '<div class="movies__not__found">Film tidak ditemukan</div>'
-    }
-
-    document.querySelector('.movies').innerHTML = html
-
-    document.getElementById('movie-search-container')
-      .dispatchEvent(new Event('movies:searched:updated'))
-  }
-}
 
 describe('Searching movies', () => {
   let presenter
