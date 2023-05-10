77fe72f35bf261d7003c08c3f808da3a44a152c0
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-10 13:32:11 +0700

    Refactor code with updated prop

--- src/scripts/views/pages/liked-movies/favorite-movie-show-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-show-presenter.js
@@ -1,10 +1,18 @@
 class FavoriteMovieShowPresenter {
-  constructor ({ view }) {
+  constructor ({
+    view,
+    favoriteMovies
+  }) {
     this._view = view
+    this._favoriteMovies = favoriteMovies
+
+    const movies = this._favoriteMovies.getAllMovies()
+
+    this._displayMovies(movies)
   }
 
   _displayMovies (movies) {
-
+    this._view.showFavoriteMovies(movies)
   }
 }
 
