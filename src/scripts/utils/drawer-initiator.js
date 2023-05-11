0c3f3d98a6c747d7d25de42091eea112002700b3
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-11 19:48:47 +0700

    Lint source code

--- src/scripts/utils/drawer-initiator.js
+++ src/scripts/utils/drawer-initiator.js
@@ -1,5 +1,5 @@
 const DrawerInitiator = {
-  init({ button, drawer, content }) {
+  init ({ button, drawer, content }) {
     button.addEventListener('click', (event) => {
       this._toggleDrawer(event, drawer)
     })
