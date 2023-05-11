23405f56a50f83655b29fa6aa08aec8190ad0893
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-11 19:16:09 +0700

    Refactor `like.js` + Add `FavoriteMovieSearchView` + Add `FavoriteMovieSearchPresenter` + Add `FavoriteMovieShowPresenter`

--- src/scripts/views/pages/like.js
+++ src/scripts/views/pages/like.js
@@ -1,22 +1,21 @@
 import FavoriteMovieIdb from '../../data/favorite-movie-idb'
 import { createMovieItemTemplate } from '../templates/template-creator'
 
+import FavoriteMovieSearchView from './liked-movies/favorite-movie-search-view'
+import FavoriteMovieSearchPresenter from './liked-movies/favorite-movie-search-presenter'
+import FavoriteMovieShowPresenter from './liked-movies/favorite-movie-show-presenter'
+
+const view = new FavoriteMovieSearchView()
+
 const Like = {
   async render () {
-    return `
-      <div class="content">
-        <h2 class="content__heading">Your Liked Movie</h2>
-        <div id="movies" class="movies"></div>
-      </div>
-    `
+    return view.getTemplate()
   },
   async afterRender () {
-    const movies = await FavoriteMovieIdb.getAllMovies()
-    const moviesContainer = document.querySelector('#movies')
-
-    movies.forEach((movie) => {
-      moviesContainer.innerHTML += createMovieItemTemplate(movie)
-    })
+    // eslint-disable-next-line no-new
+    new FavoriteMovieShowPresenter({ view, favoriteMovies: FavoriteMovieIdb })
+    // eslint-disable-next-line no-new
+    new FavoriteMovieSearchPresenter({ view, favoriteMovies: FavoriteMovieIdb })
   }
 }
 
